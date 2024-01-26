92febfd95ca7b42c6141711f109bcf300b9191d7
omarsanchez <omarcore@gmail.com>
2024-01-25 20:34:31 -0600

    Agregando guitarras al state con ref

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,16 @@ import { db } from './data/guitarras'
 import Guitarra from './components/Guitarra.vue';
 
 const guitarras = ref([])
+const carrito = ref([])
 
 onMounted(() => {
   guitarras.value = db
 })
+
+const agregarCarrito = (guitarra) => {
+  guitarra.cantidad = 1
+  carrito.value.push(guitarra)
+}
 
 </script>
 
@@ -94,6 +100,7 @@ onMounted(() => {
       <Guitarra 
         v-for="guitarra in guitarras"
         :guitarra="guitarra" 
+        @agregar-carrito="agregarCarrito"
       />
 
     </div>
--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,5 +1,9 @@
 <script setup>
 
+    import { ref } from 'vue';
+
+    const numero = ref(0)
+
     const props = defineProps({
         guitarra: {
             type: Object,
@@ -7,16 +11,25 @@
         }
     })
 
+    defineEmits(['agregar-carrito'])
+
 </script>
 
 <template>
     <div class="col-md-6 col-lg-4 my-4 row align-items-center">
         <div class="col-4">
-            <img class="img-fluid" :src="'/img/'+guitarra.imagen+'.jpg'" :alt="'imagen guitarra ' + guitarra.nombre">
+            <img 
+                class="img-fluid" 
+                :src="'/img/'+guitarra.imagen+'.jpg'" 
+                :alt="'imagen guitarra ' + guitarra.nombre"
+            >
         </div>
 
         <div class="col-8">
-            <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarra.nombre}}</h3>
+            <h3 class="text-black fs-4 fw-bold text-uppercase">
+                {{guitarra.nombre}}
+            </h3>
+
             <p>
                 {{ guitarra.descripcion }}
             </p>
@@ -24,7 +37,10 @@
             <p class="fw-black text-primary fs-3">
                 {{ guitarra.precio }}
             </p>
-            <button type="button" class="btn btn-dark w-100 ">Agregar al Carrito</button>
+
+            <button type="button" class="btn btn-dark w-100" @click="$emit('agregar-carrito',guitarra)">
+                Agregar al Carrito
+            </button>
         </div>
 
     </div><!-- FIN GUITARRA -->
